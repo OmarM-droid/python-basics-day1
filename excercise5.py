b5bf2434b0917a51e0fe098325fcc10e6c019554
OmarM-droid <omarmoharammm555@gmail.com>
2026-02-24 00:15:22 +1300

    excercise5.py
    Mini Bio Generator

--- excercise5.py
+++ excercise5.py
@@ -1,1 +1,4 @@
-
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+language = input("Enter your favourite programming language: ")
+print(f"Hi, my name is {name}. I am {age} years old and my favourite programming language is {language}")
